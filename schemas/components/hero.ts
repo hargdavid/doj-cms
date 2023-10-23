7e57d2faa946f2a3fc5293826ce1d431ea01ba0a
David Harg <hargdavid@gmail.com>
2023-10-23 11:22:09 +0200

    adding mobile image for hero

--- schemas/components/hero.ts
+++ schemas/components/hero.ts
@@ -21,6 +21,23 @@ export default {
       ],
     },
     {
+      type: 'image',
+      name: 'mobileImage',
+      title: 'Mobile image',
+      fields: [
+        {
+          type: 'text',
+          name: 'alt',
+          title: 'Alternative text',
+          description: `Some of your visitors cannot see images, 
+                    be they blind, color-blind, low-sighted; 
+                    alternative text is of great help for those 
+                    people that can rely on it to have a good idea of 
+                    what\'s on your page.`,
+        },
+      ],
+    },
+    {
       name: 'video',
       type: 'video',
       title: 'Video',
